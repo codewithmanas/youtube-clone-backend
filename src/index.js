d3235ba674a35f201715284eb04ce05fb3d5d256
Manas Ranjan Adha <manas865873@gmail.com>
2024-11-19 22:18:19 +0530

    add utility functions

--- src/index.js
+++ src/index.js
@@ -4,9 +4,24 @@ import dotenv from "dotenv";
 dotenv.config({ path: "./.env" });
 
 import connectDB from "./db/index.js";
+import app from "./app.js";
+
+const PORT = process.env.PORT || 3000;
 
 
-connectDB();
+connectDB()
+.then(() => { 
+    // console.log("Connected to DB");
+    app.on("error", (err) => {
+        console.error("ERROR on app: ", err);
+         throw err;
+    })
+
+    app.listen(PORT, () => console.log(`Server is running on port ${PORT}`)); 
+})
+.catch((error) => { 
+    console.log("MONGODB connection failed!!!", error);
+});
 
 
 
